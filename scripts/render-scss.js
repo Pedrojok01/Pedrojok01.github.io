21d29ec0fde4d6f052c32eb51fedfb61367113b1
Pedrojok01 <pedrojok@pm.me>
2023-09-07 08:32:47 +0700

    Fix date typo in experience section & run prettier

--- scripts/render-scss.js
+++ scripts/render-scss.js
@@ -11,24 +11,24 @@ const stylesPath = "../src/scss/styles.scss";
 const destPath = path.resolve(path.dirname(__filename), "../dist/css/styles.css");
 
 module.exports = function renderSCSS() {
-    const results = sass.renderSync({
-        data: entryPoint,
-        includePaths: [path.resolve(path.dirname(__filename), "../node_modules")]
+  const results = sass.renderSync({
+    data: entryPoint,
+    includePaths: [path.resolve(path.dirname(__filename), "../node_modules")]
+  });
+
+  const destPathDirname = path.dirname(destPath);
+  if (!sh.test("-e", destPathDirname)) {
+    sh.mkdir("-p", destPathDirname);
+  }
+
+  postcss([autoprefixer])
+    .process(results.css, { from: "styles.css", to: "styles.css" })
+    .then((result) => {
+      result.warnings().forEach((warn) => {
+        console.warn(warn.toString());
+      });
+      fs.writeFileSync(destPath, result.css.toString());
     });
-
-    const destPathDirname = path.dirname(destPath);
-    if (!sh.test("-e", destPathDirname)) {
-        sh.mkdir("-p", destPathDirname);
-    }
-
-    postcss([autoprefixer])
-        .process(results.css, { from: "styles.css", to: "styles.css" })
-        .then((result) => {
-            result.warnings().forEach((warn) => {
-                console.warn(warn.toString());
-            });
-            fs.writeFileSync(destPath, result.css.toString());
-        });
 };
 
 const entryPoint = `/*!
--- scripts/start-debug.js
+++ scripts/start-debug.js
@@ -4,24 +4,24 @@ const path = require("path");
 const browserSyncPath = path.resolve(path.dirname(__filename), "../node_modules/.bin/browser-sync");
 
 concurrently(
-    [
-        { command: "node --inspect scripts/sb-watch.js", name: "SB_WATCH", prefixColor: "bgBlue.bold" },
-        {
-            command: `${browserSyncPath} dist -w --no-online`,
-            name: "SB_BROWSER_SYNC",
-            prefixColor: "bgBlue.bold"
-        }
-    ],
+  [
+    { command: "node --inspect scripts/sb-watch.js", name: "SB_WATCH", prefixColor: "bgBlue.bold" },
     {
-        prefix: "name",
-        killOthers: ["failure", "success"]
+      command: `${browserSyncPath} dist -w --no-online`,
+      name: "SB_BROWSER_SYNC",
+      prefixColor: "bgBlue.bold"
     }
+  ],
+  {
+    prefix: "name",
+    killOthers: ["failure", "success"]
+  }
 ).then(success, failure);
 
 function success() {
-    console.log("Success");
+  console.log("Success");
 }
 
 function failure() {
-    console.log("Failure");
+  console.log("Failure");
 }
